efe8ea1c01fd59805742a29f099d0b398ae64873
nuoen <nuoenzhang@outlook.com>
2020-08-17 16:19:43 +0800

    second commit

--- app/src/main/java/com/nuoen/myapp/MainActivity.java
+++ app/src/main/java/com/nuoen/myapp/MainActivity.java
@@ -2,13 +2,40 @@ package com.nuoen.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.nuoen.myapp.activity.LoginActivity;
+import com.nuoen.myapp.activity.RegisterActivity;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Button login_btn;
+    private Button register_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login_btn=findViewById(R.id.btn_login);
+        register_btn=findViewById(R.id.btn_register);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(in);
+            }
+        });
+        register_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(in);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/nuoen/myapp/activity/RegisterActivity.java
@@ -0,0 +1,18 @@
+package com.nuoen.myapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nuoen.myapp.R;
+
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
+}
