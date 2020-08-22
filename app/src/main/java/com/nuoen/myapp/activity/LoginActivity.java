0110e4fd2fe2c3b4d78f53105f473d7c0241c177
nuoen <nuoenzhang@outlook.com>
2020-08-22 21:49:21 +0800

    second commit

--- app/src/main/java/com/nuoen/myapp/activity/LoginActivity.java
+++ app/src/main/java/com/nuoen/myapp/activity/LoginActivity.java
@@ -1,8 +1,16 @@
 package com.nuoen.myapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.BaseAdapter;
 import android.widget.Button;
@@ -11,6 +19,14 @@ import android.widget.Toast;
 import com.nuoen.myapp.R;
 
 import com.nuoen.myapp.util.StringUtils;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.nuoen.myapp.util.AppConfig.BASE_URL;
 
 public class LoginActivity extends BaseActivity {
     private EditText etAccount;
@@ -46,6 +62,41 @@ public class LoginActivity extends BaseActivity {
             showToast("请输入密码");
             return;
         }
+        //第一步创建OkHttpClient
+        OkHttpClient client =new OkHttpClient.Builder().build();
+        Map m=new HashMap();
+        m.put("mobile","demoData");
+        m.put("password","demoData");
+        JSONObject jsonObject=new JSONObject(m);
+        String jsonStr=jsonObject.toString();
+        RequestBody requestBodyJson=RequestBody.create(MediaType.parse("application/json;charset=utf-8"),jsonStr);
+        //创建Request
+        Request request= new Request.Builder()
+                .url(BASE_URL+"/myapp/Login")
+                .addHeader("contentType","application/json;charset=UTF-8")
+                .post(requestBodyJson)
+                .build();
+        //第四步创建call回调对象
+        final Call call=client.newCall(request);
+        //第五步发出请求
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e("onFailure",e.getMessage());
 
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String result=response.body().string();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        showToast(result);
+                    }
+                });
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/nuoen/myapp/util/AppConfig.java
@@ -0,0 +1,5 @@
+package com.nuoen.myapp.util;
+
+public class AppConfig {
+    public static final String BASE_URL="http://104.168.198.122:8080";
+}
